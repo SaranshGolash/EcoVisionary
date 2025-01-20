9d567f7d09e9349135a052ed4fe7a22fff074916
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2025-01-20 20:27:35 +0530

    Updated index.ejs(added onclick function for login button), server.js(route handling for index.ejs to login.ejs), Created login.ejs and login.css files.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import axios from "axios";
-
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static("public"));
+app.use('/Public', express.static(path.join(__dirname, 'Public')));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
 });
 
 app.listen(port, () => {
