11446dd3828ca52ce5e84536836fa4f1a6df5697
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2025-01-22 16:49:33 +0530

    Updated login.ejs,index.ejs,support.ejs and server.ejs files (all having minor changes). Moreover created signup.ejs, about.ejs and about.css files

--- server.js
+++ server.js
@@ -8,13 +8,31 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use('/Public', express.static(path.join(__dirname, 'Public')));
 
+// Home Page Route
+
 app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
 
+// Login Page Route
+
 app.get("/login", async (req, res) => {
     res.render("login.ejs");
 });
+
+// Sign-Up Page Route
+
+app.get("/signUp", async(req, res) => {
+    res.render("signup.ejs");
+});
+
+// About Us Page Route
+
+app.get("/about", async(req, res) => {
+    res.render("about.ejs");
+});
+
+// Support Page Route
 
 app.get("/support", async (req, res) => {
     res.render("support.ejs");
