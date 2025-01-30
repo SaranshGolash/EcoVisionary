11798a38b83073280f2d5d32386c8f5e99932b98
PranjeetGoswami <prgoswami2020@gmail.com>
2025-01-30 12:17:05 +0530

    added login logout functionalities and add to cart functionality, added checkout page and cart page

--- server.js
+++ server.js
@@ -1,61 +1,75 @@
 import express from "express";
-import axios from "axios";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 const app = express();
-const port = 3000;
+const port = 4000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use('/Public', express.static(path.join(__dirname, 'Public')));
+app.use("/Public", express.static(path.join(__dirname, "Public")));
+
 
 // Home Page Route
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs");
 });
+
+// Cart Page Route
+app.get("/cart", async(req,res)=> {
+  res.render("cart.ejs")
+})
+
+// checkout Page Route
+app.get("/checkout", async(req,res)=> {
+  res.render("checkout.ejs")
+})
+
+// Dashboard Page Route
+app.get("/dashboard", async(req, res)=> {
+  res.render("dashboard.ejs")
+})
 
 // Login Page Route
 
 app.get("/login", async (req, res) => {
-    res.render("login.ejs");
+  res.render("login.ejs");
 });
 
 // Sign-Up Page Route
-
-app.get("/signUp", async(req, res) => {
-    res.render("signup.ejs");
+app.get("/signup", async (req, res) => {
+  res.render("signup.ejs");
 });
 
 // About Us Page Route
 
-app.get("/about", async(req, res) => {
-    res.render("about.ejs");
+app.get("/about", async (req, res) => {
+  res.render("about.ejs");
 });
 
 // Products Page Route
 
-app.get("/products", async(req, res) => {
-    res.render("products.ejs");
-})
+app.get("/products", async (req, res) => {
+  res.render("products.ejs");
+});
 
 // Support Page Route
 
 app.get("/support", async (req, res) => {
-    res.render("support.ejs");
+  res.render("support.ejs");
 });
 
 // Services Page Route
 
 app.get("/services", async (req, res) => {
-    res.render("services.ejs");
+  res.render("services.ejs");
 });
 
 // Home Page Route
 
 app.get("/home", async (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
